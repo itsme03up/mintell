a3d6a477e0342406f132b69e73b1798791b7ddd0
itsme03up <itsme03anna.yanchuk@ognivo.net>
2025-06-11 10:36:12 +0900

    refactor: remove turbopack configuration and adjust serverActions in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  turbopack: {
-    // Your turbopack options here (or leave as empty object)
-  },
   experimental: {
-    serverActions: true,
+    serverActions: {},
   },
 }
 
